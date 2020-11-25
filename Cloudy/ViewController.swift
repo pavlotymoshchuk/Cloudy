40f63dd105ad1398ad2da5971e8ff593e25d6f9d
Pavlo Tymoshchuk <Pavlo25575@icloud.com>
2020-11-25 16:59:24 +0200

    Added upload photos from camera and gallery

--- Cloudy/ViewController.swift
+++ Cloudy/ViewController.swift
@@ -7,64 +7,34 @@
 
 import UIKit
 import Photos
-class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+class ViewController: UIViewController {
     
-    var imagePickerController: UIImagePickerController!
-    var imageView: UIImageView!
+    var backgroundImageView: UIImageView!
     var currentImage = UIImage()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imagePickerController = UIImagePickerController()
-        imagePickerController.delegate = self;
-    }
-    @IBAction func uploadImageButton(_ sender: UIButton) {
-        openPhotoGallery()
-        addPhotoToImageView()
+        self.view.insertSubview(imageForBackground(), at: 0)
     }
     
-    func openPhotoGallery() {
-        PHPhotoLibrary.requestAuthorization { (status) in
-                switch status {
-                case .authorized:
-                    print("You Are Authrized To Access")
-                    let fetchOptions = PHFetchOptions()
-                    let allPhotos = PHAsset.fetchAssets(with: .image, options: fetchOptions)
-                    print("Found number of:  \(allPhotos.count) images")
-                case .denied, .restricted:
-                    print("Not allowed")
-                case .notDetermined:
-                    print("Not determined yet")
-                case .limited:
-                    print("You Are Authrized To Access")
-                    let fetchOptions = PHFetchOptions()
-                    let allPhotos = PHAsset.fetchAssets(with: .image, options: fetchOptions)
-                    print("Found number of:  \(allPhotos.count) images")
-                @unknown default:
-                    print("Not allowed")
-                }
-            }
-        self.imagePickerController.sourceType =  UIImagePickerController.SourceType.photoLibrary
-        self.present(self.imagePickerController, animated: true, completion: nil)
+    @IBAction func uploadImageButton(_ sender: Any) {
+        ImagePickerManager().pickImage(self){ image in
+            self.currentImage = image
+            self.addPhotoToBackground()
+        }
+    }
+
+    
+    func addPhotoToBackground() {
+        backgroundImageView.image = currentImage
     }
     
-    func addPhotoToImageView() {
-        imageView = UIImageView()
-        imageView.frame.size = self.view.frame.size
-        imageView.contentMode = .scaleAspectFill
-        imageView.image = currentImage
-        self.view.addSubview(imageView)
-    }
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
-        currentImage = image
-        dismiss(animated: true, completion: nil)
-        
-    }
-        
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
+    func imageForBackground() -> UIImageView {
+        backgroundImageView = .init(frame: self.view.frame)
+        backgroundImageView.image = currentImage
+        backgroundImageView.contentMode = .scaleAspectFit
+        return backgroundImageView
     }
 
     
