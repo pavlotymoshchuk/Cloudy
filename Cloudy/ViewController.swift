65b5ba8f015b6ef645c0b8de741357861786930b
Pavlo Tymoshchuk <Pavlo25575@icloud.com>
2021-04-30 13:14:03 +0300

    Added cloud percentage calculation

--- Cloudy/ViewController.swift
+++ Cloudy/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Photos
+
 class ViewController: UIViewController {
     
     var imageView: UIImageView!
@@ -70,9 +71,11 @@ class ViewController: UIViewController {
             UIView.animate(withDuration: 0.05) {
                 self.skyImageView.transform = CGAffineTransform.identity
                 let imageToDownload = UIImage(data: self.skyImageView.image!.pngData()!)!
-                UIImageWriteToSavedPhotosAlbum(imageToDownload, self, nil, nil)
-                UIImageWriteToSavedPhotosAlbum(imageToDownload.withAdjustment(bySaturationVal: 0, byContrastVal: 1.35), self, nil, nil)
-                self.alert(alertTitle: "Saved", alertMessage: "Image was saved", alertActionTitle: "OK")
+//                UIImageWriteToSavedPhotosAlbum(imageToDownload, self, nil, nil)
+//                let imageWithAdjustmentToDownload = imageToDownload.withAdjustment(bySaturationVal: 0, byContrastVal: 1.35)
+//                UIImageWriteToSavedPhotosAlbum(imageWithAdjustmentToDownload, self, nil, nil)
+                let cloudPercentage = self.getAverageColorOfImage(image: imageToDownload)!
+                self.alert(alertTitle: "Saved", alertMessage: "Image was saved" + "\n" + "Cloud Percentage " + String(cloudPercentage) + "%", alertActionTitle: "OK")
             }
         default: break
         }
@@ -89,10 +92,106 @@ class ViewController: UIViewController {
     }
 
     
+    func getAverageColorOfImage(image: UIImage) -> Float? /*-> (UInt8, UInt8, UInt8, Float)?*/ {
+        guard let cgImage = image.cgImage else { return nil }
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
+        bitmapInfo |= CGImageAlphaInfo.premultipliedLast.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
+        let width = Int(image.size.width)
+        let height = Int(image.size.height)
+        let bytesPerRow = width * 4
+        let imageData = UnsafeMutablePointer<Pixel>.allocate(capacity: width * height)
+        guard let imageContext = CGContext(
+            data: imageData,
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: bytesPerRow,
+            space: colorSpace,
+            bitmapInfo: bitmapInfo
+        ) else { return nil }
+        imageContext.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        let pixels = UnsafeMutableBufferPointer<Pixel>(start: imageData, count: width * height)
+
+        var cloudPercentage: Float = 0
+        print(width,height)
+        
+        var pixelCount = 0
+        var cloudPixelCount = 0
+
+        for y in 0..<height {
+            for x in 0..<width {
+                let index = y * width + x
+                let pixel = pixels[index]
+                if pixel.alpha != 0 {
+//                    print(pixel.red,pixel.green,pixel.blue)
+                    if calculateVSC(pixel: pixel) < 0 {
+                        cloudPixelCount += 1
+                    }
+                    pixelCount += 1
+                }
+            }
+        }
+        cloudPercentage = Float(cloudPixelCount) / Float(pixelCount) * 100
+        print(cloudPercentage,"%")
+        
+        return cloudPercentage
+    }
+    
+    func calculateVSC(pixel: Pixel) -> Float {
+        var vsc: Float = 0
+        let r = Float(pixel.red)/255
+        let g = Float(pixel.green)/255
+        let b = Float(pixel.blue)/255
+        let v = ([r,g,b].max()! + [r,g,b].min()!)/2
+        let scyl: Float = ([r,g,b].max()! - [r,g,b].min()!) == 0 ? 0 : ([r,g,b].max()!-[r,g,b].min()!)/(1-abs(2*v-1))
+        let scone:Float = 0
+        let br = (b-r)/(b+r)
+            
+//        vsc = -6.28*r + 0.454*g - 4.11*b - 1.81*scyl - 4.04*scone + 8.88*v + 1.53*br + 0.586
+        vsc = -6.28*r + 0.454*g - 4.11*b - 2.5*scyl + 8.88*v + 1.53*br + 0.586
+        return vsc
+    }
+    
+}
+
+public struct Pixel {
+    public var value: UInt32
+    
+    public var red: UInt8 {
+        get {
+            return UInt8(value & 0xFF)
+        } set {
+            value = UInt32(newValue) | (value & 0xFFFFFF00)
+        }
+    }
+    
+    public var green: UInt8 {
+        get {
+            return UInt8((value >> 8) & 0xFF)
+        } set {
+            value = (UInt32(newValue) << 8) | (value & 0xFFFF00FF)
+        }
+    }
+    
+    public var blue: UInt8 {
+        get {
+            return UInt8((value >> 16) & 0xFF)
+        } set {
+            value = (UInt32(newValue) << 16) | (value & 0xFF00FFFF)
+        }
+    }
+    
+    public var alpha: UInt8 {
+        get {
+            return UInt8((value >> 24) & 0xFF)
+        } set {
+            value = (UInt32(newValue) << 24) | (value & 0x00FFFFFF)
+        }
+    }
 }
 
 extension UIImage {
-    
     func withAdjustment(bySaturationVal: CGFloat, byContrastVal: CGFloat) -> UIImage {
         guard let cgImage = self.cgImage else { return self }
         guard let filter = CIFilter(name: "CIColorControls") else { return self }
@@ -104,5 +203,4 @@ extension UIImage {
         let image = UIImage(cgImage: newCgImage, scale: UIScreen.main.scale, orientation: imageOrientation)
         return UIImage(data: image.pngData()!)!
     }
-
 }
